0716a589bb1266d33bbfd41c60b786f32a3e4322
catmons <tingl32145@gmail.com>
2024-12-18 16:12:53 -0500

    login and signup frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
+import LoginPage from './login';
+import SignUp from './signup';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 const App = () => {
-
-    
-
-    return (
-        <div className="app-container">
-            <header>
-                <h1>Welcome to Moodie Buddy</h1>
-            </header>
-                </div>
-    );
+  return (
+    <Router>
+      <div className='app-container'>
+        <header>
+          <h1>Welcome to Moodie Buddy</h1>
+        </header>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
